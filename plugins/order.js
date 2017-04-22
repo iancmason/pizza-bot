1ca0c304089745c031e3ed998672cd6186094899
Tracy Tran <tracyctran@gmail.com>
2017-04-22 10:05:25 -0400

    add order plugin, require in index.js

---
+++ plugins/order.js
@@ -0,0 +1,11 @@
+module.exports = {
+  init: (controller) => {
+    controller.hears([/I want a pizza/], ['direct_message', 'direct_mention'], (bot, message) => {
+      bot.reply(message, `Hi there! I"m looking for nearby stores.`)
+    })
+  },
+  help: {
+    command: 'pizza',
+    text: `Say "I want a pizza" and I will order you one.`
+  }
+}
