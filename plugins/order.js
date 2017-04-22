05f70e9b2db1df649d6a94da48e023b7d5cbc7e1
Tracy Tran <tracyctran@gmail.com>
2017-04-22 11:30:16 -0400

    pizza bot returning list in slack

--- plugins/order.js
+++ plugins/order.js
@@ -1,11 +1,29 @@
+const pizzapi = require('dominos');
+
 module.exports = {
   init: (controller) => {
-    controller.hears([/I want a pizza/i], ['direct_message', 'direct_mention'], (bot, message) => {
-      bot.reply(message, `Hi there! I"m looking for nearby stores.`)
-    })
+    controller.hears([/I want a pizza/i], ['direct_message', 'direct_mention'], getPizza)
   },
   help: {
     command: 'pizza',
     text: `Say "I want a pizza" and I will order you one.`
   }
+}
+
+function getPizza (bot, message) {
+  bot.reply(message, `Hi there! I"m looking for nearby stores.`);
+  pizzapi.Util.findNearbyStores(
+  '11 Times Square, New York, NY, 10036',
+  'Delivery',
+  function (storeData) {
+    const filteredStoreData = storeData.result.Stores.filter((store) =>
+      store.IsOpen && store.IsOnlineCapable && store.IsOnlineNow
+    )
+    const reply = filteredStoreData.map((store) =>
+      `${store.StoreId} \r\n ${store.AddressDescription} \r\n`
+    )
+    reply.forEach(function(reply) {
+      bot.reply(message, reply)
+    })
+  })
 }
